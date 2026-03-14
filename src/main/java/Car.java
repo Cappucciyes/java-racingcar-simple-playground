0f6a3da05c209381e7a9e6b836b78db55686d424
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-03-15 01:32:30 +0900

    feat : Car 클래스의 name 필드 및 getter 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,17 +1,23 @@
 public class Car {
     public int distance = 0;
+    private String name;
     private NumberGeneratorImpl numberGenerator;
 
-    Car() {
-       this.numberGenerator = new NumberGeneratorImpl();
+    Car(String name) {
+        this.name = name;
+        this.numberGenerator = new NumberGeneratorImpl();
     }
-    Car(NumberGeneratorImpl numberGenerator) {
-       this.numberGenerator = numberGenerator;
+    Car(String name, NumberGeneratorImpl numberGenerator) {
+        this.name = name;
+        this.numberGenerator = numberGenerator;
     }
 
     public void move() {
         int flag = numberGenerator.generateNumber();
-
         if (flag >= 4) distance += 1;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
