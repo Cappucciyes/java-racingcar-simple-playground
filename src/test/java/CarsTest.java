b357e4fe24813488ffbf16e1f025edf02852bcaf
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-03-22 20:26:20 +0900

    test : implemented test `testGetRacerInfo`

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -43,5 +44,25 @@ public class CarsTest {
         List<String> winners = cars.getMaxDistanceCarNames();
         Assertions.assertEquals(1, winners.size());
         assertThat(winners).hasSameElementsAs(Arrays.asList("1"));
+    }
+
+    @Test
+    void testGetRacerInfo() {
+        // given
+        cars.addCar(new Car(Integer.toString(0), new TestNumberGeneratorImpl(3)));
+        cars.addCar(new Car(Integer.toString(0), new TestNumberGeneratorImpl(4)));
+        cars.addCar(new Car(Integer.toString(1), new TestNumberGeneratorImpl(9)));
+
+        //when
+        cars.tryMovingEveryCar();
+
+        // result
+        Map<String, List<Integer>> carState = cars.getRacerInfo();
+        Assertions.assertEquals(2, carState.size());
+
+        Assertions.assertEquals(2, carState.get("0").size());
+        assertThat(carState.get("0")).hasSameElementsAs(Arrays.asList(0,1));
+        Assertions.assertEquals(1, carState.get("1").size());
+        assertThat(carState.get("1")).hasSameElementsAs(Arrays.asList(1));
     }
 }
