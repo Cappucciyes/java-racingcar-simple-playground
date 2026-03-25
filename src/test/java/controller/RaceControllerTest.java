372bbabb93db50e818609f2e944272e8828c1234
이태규 <58386574+Cappucciyes@users.noreply.github.com>
2026-03-26 06:45:16 +0900

    test : Integration test

--- src/test/java/controller/RaceControllerTest.java
+++ src/test/java/controller/RaceControllerTest.java
@@ -3,15 +3,21 @@ package controller;
 import components.ZeroToNineCyclingGenerator;
 import model.Car;
 import model.Cars;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ValueSource;
 import view.InputView;
 import view.OutputView;
 
+import javax.print.DocFlavor;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -20,12 +26,11 @@ class RaceControllerTest {
     Cars cars = new Cars();
 
     @Test
-    @DisplayName("주어진 횟수만큼 자동차를 전진시키려 한다.")
+    @DisplayName("통합 테스트 : 주어진 횟수만큼 자동차를 전진시키려 한다.")
     protected void testPlayRace() {
         //given
-        cars.addCar(new Car("alice", new ZeroToNineCyclingGenerator(3)));
-        cars.addCar(new Car("bob", new ZeroToNineCyclingGenerator(4)));
-        Scanner scanner = new Scanner(new ByteArrayInputStream("2".getBytes()));
+        cars.addCar(new Car("alice", new ZeroToNineCyclingGenerator(4)));
+        Scanner scanner = new Scanner(new ByteArrayInputStream("3".getBytes()));
         raceController = new RaceController(new InputView(scanner), new OutputView(), cars);
 
         //when
@@ -33,8 +38,18 @@ class RaceControllerTest {
 
         //then
         Map<String, List<Integer>> raceResult = cars.getRacerInfo();
-        assertEquals(2, raceResult.size());
-        assertEquals(List.of(1), raceResult.get("alice"));
-        assertEquals(List.of(2), raceResult.get("bob"));
+        assertEquals(1, raceResult.size());
+        assertEquals(List.of(3), raceResult.get("alice"));
+    }
+
+    @Test
+    @DisplayName("통합 테스트 : RaceController가 Cars를 생성하는데 문제가 없다")
+    protected void raceControllerCanBuildItsOwnCarsWithUserInput() {
+        //given
+        Scanner scanner = new Scanner(new ByteArrayInputStream("alice,bob\n3\n".getBytes()));
+        raceController = new RaceController(new InputView(scanner), new OutputView());
+
+        //when
+        raceController.playRace();
     }
 }
